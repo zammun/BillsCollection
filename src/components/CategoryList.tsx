9af27c877f41cdd4ec1e98990c6cd5a64a14fab6
muneeb <muneebzaman6@gmail.com>
2024-06-26 08:59:28 -0400

    adding pics

--- src/components/CategoryList.tsx
+++ src/components/CategoryList.tsx
@@ -8,25 +8,25 @@ const CategoryList = () => {
             <div className='flex gap-4 md:gap-8'>
                 <Link href='/list?cat=test' className='flex-shrink-0 w-full sm:w-1/2 lg:w-1/4 xl:w-1/6'>
                     <div className='relative bg-slate-100 w-full h-96'>
-                        <Image src='/2.jpg' alt='' fill sizes='20vw' className='object-cover'/>
+                        <Image src='/10.jpg' alt='' fill sizes='20vw' className='object-cover'/>
                     </div>
                         <h1 className='mt-8 font-light text-cl tracking-wide'>Category Name</h1>
                 </Link>
                 <Link href='/list?cat=test' className='flex-shrink-0 w-full sm:w-1/2 lg:w-1/4 xl:w-1/6'>
                     <div className='relative bg-slate-100 w-full h-96'>
-                        <Image src='/2.jpg' alt='' fill sizes='20vw' className='object-cover'/>
+                        <Image src='/9.jpg' alt='' fill sizes='20vw' className='object-cover'/>
                     </div>
                         <h1 className='mt-8 font-light text-cl tracking-wide'>Category Name</h1>
                 </Link>
                 <Link href='/list?cat=test' className='flex-shrink-0 w-full sm:w-1/2 lg:w-1/4 xl:w-1/6'>
                     <div className='relative bg-slate-100 w-full h-96'>
-                        <Image src='/2.jpg' alt='' fill sizes='20vw' className='object-cover'/>
+                        <Image src='/5.jpg' alt='' fill sizes='20vw' className='object-cover'/>
                     </div>
                         <h1 className='mt-8 font-light text-cl tracking-wide'>Category Name</h1>
                 </Link>
                 <Link href='/list?cat=test' className='flex-shrink-0 w-full sm:w-1/2 lg:w-1/4 xl:w-1/6'>
                     <div className='relative bg-slate-100 w-full h-96'>
-                        <Image src='/2.jpg' alt='' fill sizes='20vw' className='object-cover'/>
+                        <Image src='/11.jpg' alt='' fill sizes='20vw' className='object-cover'/>
                     </div>
                         <h1 className='mt-8 font-light text-cl tracking-wide'>Category Name</h1>
                 </Link>
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,7 +5,7 @@ const Footer = () => {
     return (
         <div className='py-24 px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64 bg-gray-100 text-sm mt-24'>
             {/* Top */}
-            <div className='flex justify-between gap-24'>
+            <div className='flex flex-col md:flex-row justify-between gap-24'>
                 {/* Left */}
                 <div className='w-full md:w-1/2 lg:w-1/4 flex flex-col gap-8'>
                     <Link href='/'> 
@@ -21,14 +21,67 @@ const Footer = () => {
                     </div>
                 </div>
                 {/* Center */}
-                <div className='hidden lg:flex justify-between w-1/2'></div>
+                <div className='hidden lg:flex justify-between w-1/2'>
+                <div className='flex flex-col justify-between'>
+                    <h1 className='font-medium text-lg'>COMPANY</h1>
+                    <div className='flex flex-col gap-6'>
+                        <Link href=''>About Us</Link>
+                        <Link href=''>Careers</Link>
+                        <Link href=''>Affiliates</Link>
+                        <Link href=''>Contact Us</Link>
+                    </div>
+                </div>
+                <div className='flex flex-col justify-between'>
+                    <h1 className='font-medium text-lg'>SHOP</h1>
+                    <div className='flex flex-col gap-6'>
+                        <Link href=''>New Arrivals</Link>
+                        <Link href=''>Accessories</Link>
+                        <Link href=''>All Products</Link>
+                    </div>
+                </div>
+                <div className='flex flex-col justify-between'>
+                    <h1 className='font-medium text-lg'>HELP</h1>
+                    <div className='flex flex-col gap-6'>
+                        <Link href=''>Customer Service</Link>
+                        <Link href=''>My Account</Link>
+                        <Link href=''>Find a Store</Link>
+                    </div>
+                </div>
+                </div>
                 {/* Right */}
                 <div className='w-full md:w-1/2 lg:w-1/4 flex flex-col gap-8'>
-                <h1>SUBSCRIBE</h1>
+                <h1 className='font-medium text-lg'>SUBSCRIBE</h1>
+                <p>
+                    Be the first to get updates and offers from Bills Collection, and teasers about our latest designs.
+                </p>
+                <div className='flex'>
+                    <input type='text' placeholder='Email address' className='p-4 w-3/4' />
+                    <button className='w-1/4 bg-lama text-white'>JOIN</button>
+                </div>
+                    <span className='font-semibold'>Secure Payments</span>
+                    <div className='flex justify-between'>
+                        <Image src='/visa.png' alt='' width={40} height={20} />
+                        <Image src='/mastercard.png' alt='' width={40} height={20} />
+                        <Image src='/paypal.png' alt='' width={40} height={20} />
+                        <Image src='/discover.png' alt='' width={40} height={20} />
+                        <Image src='/skrill.png' alt='' width={40} height={20} />
+                    </div>
                 </div>
             </div>
             {/* Bottom */}
-            <div className=''></div>
+            <div className='flex flex-col md:flex-row items-center justify-between gap-8 mt-16'>
+                <div className=''>© 2024 Bills Collection</div>
+                <div className='flex flex-col gap-8 md:flex-row'>
+                    <div className=''>
+                        <span className='text-gray-500 mr-4'>Language</span>
+                        <span className='font-medium'>United States | English</span>
+                    </div>
+                    <div className=''>
+                        <span className='text-gray-500 mr-4'>Currency</span>
+                        <span className='font-medium'>USD</span>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
