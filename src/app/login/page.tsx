34d014f7e8d3e3f8f903f8d720f5f2467ca35dd2
muneeb <muneebzaman6@gmail.com>
2025-02-03 12:07:38 +0500

    updating login

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,6 +1,39 @@
 const LoginPage = () => {
     return (
-        <div className=''>LoginPage</div>
+        <div className='login-container'>
+            <h2 className='form-title'>Login with</h2>
+            <div className='social-login'>
+                <button className='social-button'>
+                    <img src='/google.svg' alt='Google' className='social-icon' />
+                </button>
+                <button className='social-button'>
+                    <img src='/apple.svg' alt='Apple' className='social-icon' />
+                    Apple
+                </button>
+        </div>
+        <p className='separator'><span>or</span></p>
+
+        <form action='#' className='login-form'>
+            <div className='input-wrapper'>
+                <input type='email' placeholder='Email'
+                className='input-field' required />
+                <i className='material-symbols-rounded'>mail</i>
+            </div>
+
+            <div className='input-wrapper'>
+                <input type='password' placeholder='Password'
+                className='input-field' required />
+                <i className='material-symbols-rounded'>lock</i>
+            </div>
+            <a href='#' className='forgot-password'>Forgot password?</a>
+
+            <button className='login-button'>Login</button>
+        </form>
+
+        <p className='signup-text'>Don&apos;t have an account?
+            <a href='#' className='signup-link'>Sign up</a>
+        </p>
+    </div>
     )
 }
 
